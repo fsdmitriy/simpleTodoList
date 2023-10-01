edba890829cb188e3e9ea1378dc760a5e17c8b52
Dmitriy <dmitriy.fokasii@gmail.com>
2023-10-01 19:48:28 +0300

    code cleanup

--- SimpleTODOList/Controllers/TodoController.cs
+++ SimpleTODOList/Controllers/TodoController.cs
@@ -76,6 +76,7 @@ public class TodoController : ControllerBase
         {
             return BadRequest();
         }
+
         return NoContent();
     }
 }
--- SimpleTODOList/Program.cs
+++ SimpleTODOList/Program.cs
@@ -18,19 +18,18 @@ builder.Services.AddDbContext<TodoContext>(options =>
     options.EnableDetailedErrors();
 });
 
-//var dbContext = builder.Services?.BuildServiceProvider().GetService<TodoContext>();
-//dbContext.Database.EnsureCreated();
-//dbContext.Database.Migrate();
+var dbContext = builder.Services?.BuildServiceProvider().GetService<TodoContext>();
+if (dbContext is not null)
+{
+    dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
+}
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services!.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-var dbContext = app.Services.GetRequiredService<TodoContext>();
-dbContext.Database.EnsureCreated();
-dbContext.Database.Migrate();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
